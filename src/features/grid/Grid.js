90be7ce9cdbcbe77563c7aaeecb045af400dca04
Paul Nguon <herropaul@gmail.com>
2022-09-21 22:58:58 -0700

    added conditional rendering to display grid

--- src/features/grid/Grid.js
+++ src/features/grid/Grid.js
@@ -1,20 +1,25 @@
-import React from 'react'
-import { Flex, Image, Text } from '@chakra-ui/react'
-import { useSelector } from 'react-redux'
+import React from "react";
+import { Flex, Image, Text } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 
 export default function Grid() {
-
-  const artists = useSelector(state => state.artistsSlice.artists)
-  const mainArtist = artists[0] // Artist object
+  const artists = useSelector((state) => state.artistsSlice.artists);
+  const mainArtist = artists[0]; // Artist object
 
   return (
-    <Flex bg="blue.100" direction="column" >
-        {/* {artists?.map((artist) => 
-          <Flex>{artist.name}</Flex>
-        ) } */}
-        <Image boxSize="150px" borderRadius="full" src={mainArtist.images[0].url}/>
-        <Text fontSize="3xl">{mainArtist.name}</Text>
-
+    <Flex bg="blue.100" direction="column" alignItems="center">
+      {artists.length ? (
+        <Flex>
+          <Image
+            boxSize="150px"
+            borderRadius="full"
+            src={mainArtist.images[0].url}
+          />
+          <Text fontSize="3xl">{mainArtist.name}</Text>
+        </Flex>
+      ) : (
+        <Flex>No worky</Flex>
+      )}
     </Flex>
-  )
+  );
 }
--- src/features/search/searchSlice.js
+++ src/features/search/searchSlice.js
@@ -1,58 +1,61 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import SpotifyWebApi from 'spotify-web-api-js';
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import SpotifyWebApi from "spotify-web-api-js";
 
 const client_id = process.env.REACT_APP_CLIENT_ID;
 const client_secret = process.env.REACT_APP_CLIENT_SECRET;
 
 const initialState = {
-    loading: false,
-    artists: [],
-    error: ''
-}
+  loading: false,
+  artists: [],
+  error: "",
+};
 
-const fetchRequest = async () => {
-  return await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
+const fetchRequest = () => {
+  return fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-        Authorization: 'Basic ' + btoa(client_id + ':' + client_secret),
+      "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      Authorization: "Basic " + btoa(client_id + ":" + client_secret),
     },
-    body: 'grant_type=client_credentials',
-  })
-}
+    body: "grant_type=client_credentials",
+  });
+};
 
-export const getArtistByQuery = createAsyncThunk('search/getArtistByQuery', async (query) => {
-    return await fetchRequest()
-        .then((result) => result.json())
-        .then((data) => {
-          const token = data?.access_token
-          const spotify = new SpotifyWebApi();
+export const getArtistByQuery = createAsyncThunk(
+  "search/getArtistByQuery",
+  (query) => {
+    return fetchRequest()
+      .then((result) => result.json())
+      .then((data) => {
+        const token = data?.access_token;
+        const spotify = new SpotifyWebApi();
 
-          spotify.setAccessToken(token);
-          return spotify.searchArtists(query, {limit: 10})
-        })
-        .then((response) => response?.artists?.items)
-})
+        spotify.setAccessToken(token);
+        return spotify.searchArtists(query, { limit: 10 });
+      })
+      .then((response) => response?.artists?.items);
+  }
+);
 
 const artistsSlice = createSlice({
-    name: 'artists',
-    initialState,
-    extraReducers: builder => {
-      builder.addCase(getArtistByQuery.pending, state => {
-        state.loading = true
-      })
-      builder.addCase(getArtistByQuery.fulfilled, (state, action) => {
-        state.loading = false
-        console.log("Payload: " + action.payload)
-        state.artists = action.payload
-        state.error = ''
-      })
-      builder.addCase(getArtistByQuery.rejected, (state, action) => {
-        state.loading = false
-        state.artists = []
-        state.error = action.error.message
-      })
-    }
-    })
+  name: "artists",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getArtistByQuery.pending, (state) => {
+      state.loading = true;
+    });
+    builder.addCase(getArtistByQuery.fulfilled, (state, action) => {
+      state.loading = false;
+      console.log("Payload: " + action.payload);
+      state.artists = action.payload;
+      state.error = "";
+    });
+    builder.addCase(getArtistByQuery.rejected, (state, action) => {
+      state.loading = false;
+      state.artists = [];
+      state.error = action.error.message;
+    });
+  },
+});
 
-export default artistsSlice.reducer
+export default artistsSlice.reducer;
