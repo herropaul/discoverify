f186bc115b90a588014dad97ae1912515238111a
Paul Nguon <herropaul@gmail.com>
2022-09-21 21:43:57 -0700

    fixing Grid component

--- src/features/grid/Grid.js
+++ src/features/grid/Grid.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
+import { Flex, Image, Text } from '@chakra-ui/react'
 import { useSelector } from 'react-redux'
 
 export default function Grid() {
 
   const artists = useSelector(state => state.artistsSlice.artists)
+  const mainArtist = artists[0] // Artist object
 
   return (
-    <Flex bg="blue.100" direction="column" textAlign="center">
-        {artists?.map((artist) => 
+    <Flex bg="blue.100" direction="column" >
+        {/* {artists?.map((artist) => 
           <Flex>{artist.name}</Flex>
-        ) }
+        ) } */}
+        <Image boxSize="150px" borderRadius="full" src={mainArtist.images[0].url}/>
+        <Text fontSize="3xl">{mainArtist.name}</Text>
+
     </Flex>
   )
 }
