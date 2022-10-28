bee598044d1671acd5432d86d443f05b2c348dfe
Paul Nguon <herropaul@gmail.com>
2022-10-27 23:38:28 -0700

    tracklist maps through each tracks from the user input and displays its appropriated data

---
+++ src/features/grid/TrackCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Flex, Text, Image } from '@chakra-ui/react'
+
+export default function TrackCard({key, image, trackName, artistName}) {
+  return (
+    <Flex className=" bg-green-300 py-3 px-3 my-2">
+        <Image boxSize="75px" objectFit="cover" src={image} />
+        <Flex className=' flex flex-col mx-5 font-mono'>
+            <Text className=''>{trackName}</Text>
+            <Text className=' text-xs'> {artistName} </Text>
+        </Flex>
+    </Flex>
+  )
+}
--- src/features/grid/TrackList.js
+++ src/features/grid/TrackList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import TrackCard from './TrackCard'
 import { Flex, Text, Image } from '@chakra-ui/react'
 
 export default function TrackList({artists, tracks}) {
@@ -6,21 +7,24 @@ export default function TrackList({artists, tracks}) {
   //const [mainArtist, setMainArtist] = useState("");
 
   return (
-    <Flex className="bg-red-200" 
+    <Flex overflowX="scroll" className="bg-red-200" 
     h="32rem" w="25rem"
     mt={5} ml="5rem"
     >
-      {artists.length ? 
+      {artists.length && tracks.length ? 
       (
         <div className=' bg-red-300 w-full '>
         <Text className="mt-3 font-mono font-bold text-xl mb-5">Tracks</Text>
-        <Flex className=" bg-green-300 py-3 px-3 my-2">
-          <Image boxSize="75px" objectFit="cover" src={tracks[0]?.album?.images[0].url} />
-          <Flex className=' flex flex-col mx-5 font-mono'>
-            <Text className=''>{tracks[0].name}</Text>
-            <Text className=' text-xs'> {artists[0].name} </Text>
-          </Flex>
-        </Flex>
+        {tracks.map((track) => {
+          if(track.album.images === 0){
+            return(
+              <TrackCard key={track.id} image="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg" trackName={track.name} artistName={track.artists[0].name} />
+            )
+          }
+          return (
+            <TrackCard key={track.id} image={track?.album?.images[0].url} trackName={track.name} artistName={track.artists[0].name} />
+          )
+        })}
       </div>
       ) : (
         <Flex className="text-4xl font-bold">
